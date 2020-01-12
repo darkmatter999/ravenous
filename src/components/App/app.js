a052e47029e4d1c91ac839ba08baea96bde71a65
darkmatter999 <oliverhaack@protonmail.com>
2020-01-12 10:33:20 +0100

    Ravenous Project Part 2: Passing in props

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -2,7 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './app.css';
 import BusinessList from '../BusinessList/BusinessList';
+import Business from '../Business/Business';
 import SearchBar from '../SearchBar/SearchBar';
+
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+
+
+const businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business
+];
+
 
 /*
 function App() {
@@ -34,7 +58,8 @@ class App extends React.Component {
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar />
-        <BusinessList /> 
+        <BusinessList businesses={businesses} />
+        
       </div>
     )
   }
--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import './BusinessList.css';
 import Business from '../Business/Business';
+import App from '../App/app.js';
+
 
 class BusinessList extends React.Component {
     render() {
         return (
+            
             <div className="BusinessList">
-                <Business /> 
-                <Business /> 
-                <Business /> 
-                <Business />
-                <Business />
-                <Business />
-                <Business />
+                {this.props.businesses.map(business => {
+                    return <Business business = {business} />
+                })}
             </div>
-        )
+            
+         
+)
+
     }
 };
 
